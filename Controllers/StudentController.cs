b132745bf6b100cccaa22799d06c0cbbe5707f72
Jeelsavaliya <JEEL SAVALIYA@LAPTOP-V175VFGH>
2024-04-08 18:02:52 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using CRUDASPCoreWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CRUDASPCoreWebAPI.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "https://localhost:7148/api/Student/";
+        private HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Students> students = new List<Students>(); 
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<List<Students>>(result);
+                if (data != null)
+                {
+                    students = data;
+                }
+            }
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Students.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDASPCoreWebAPI.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Standard { get; set; }
+        [Required]
+        public string School { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+    }
+}
